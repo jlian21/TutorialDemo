5e7b84c0790c03c6a4b8d3b09299e7fb8aa60022
pico <z_lantern@OSXLAP03328.local>
2020-12-28 16:09:01 -0800

    backend for tutorial demo

---
+++ tutorial-demo/src/main/java/com/backend/tutorialdemo/repository/TutorialRepository.java
@@ -0,0 +1,13 @@
+package com.backend.tutorialdemo.repository;
+
+import java.util.List;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import com.backend.tutorialdemo.model.Tutorial;
+
+public interface TutorialRepository extends MongoRepository<Tutorial, String> {
+    List<Tutorial> findByTitleContaining(String title);
+
+    List<Tutorial> findByPublished(boolean published);
+}
---
+++ tutorial-demo/src/test/java/com/backend/tutorialdemo/TutorialDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.tutorialdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
