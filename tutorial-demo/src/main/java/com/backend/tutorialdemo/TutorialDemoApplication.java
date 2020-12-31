52915744a6432f9534a12e6b8dc31858a04998a5
jlian21 <j_lian@outlook.com>
2020-12-30 19:54:23 -0800

    integrate react and springboot

--- tutorial-demo/src/main/java/com/backend/tutorialdemo/TutorialDemoApplication.java
+++ tutorial-demo/src/main/java/com/backend/tutorialdemo/TutorialDemoApplication.java
@@ -2,8 +2,9 @@ package com.backend.tutorialdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class TutorialDemoApplication {
 
 	public static void main(String[] args) {
